84e5ed1c77367bb46418dcc9c85e5f3909bbd82b
abbanfahim <lloydtechno58@gmail.com>
2020-12-29 12:07:28 +0400

    changed port functionality

--- app.js
+++ app.js
@@ -78,11 +78,6 @@ app.post('/update', (req,res)=>{
 	res.redirect('/');
 });
 
-let port = process.env.PORT;
-if (port === null || port === '') {
-	port === 3000;
-};
-
-app.listen(port, ()=>{
+app.listen(3000 || process.env.PORT, ()=>{
 	console.log('Server has started');
 });
